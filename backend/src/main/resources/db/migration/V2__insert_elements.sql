508bcad5b17d53b94bd566b93bf7ad78c0257c85
Abdul Rehman <astronicle78@gmail.com>
2026-02-15 20:52:43 +0530

    Inserted static data into db

---
+++ backend/src/main/resources/db/migration/V2__insert_elements.sql
@@ -0,0 +1,9 @@
+INSERT INTO element (element_id, element_slug, element_name)
+VALUES
+    (1, 'pyro', 'Pyro'),
+    (2, 'hydro', 'Hydro'),
+    (3, 'electro', 'Electro'),
+    (4, 'cryo', 'Cryo'),
+    (5, 'anemo', 'Anemo'),
+    (6, 'geo', 'Geo'),
+    (7, 'dendro', 'Dendro');
---
+++ backend/src/main/resources/db/migration/V3__insert_weapon_types.sql
@@ -0,0 +1,7 @@
+INSERT INTO wep_type (wep_type_id, wep_type_slug, wep_type_name)
+VALUES
+    (1, 'sword',     'Sword'),
+    (2, 'claymore',  'Claymore'),
+    (3, 'polearm',   'Polearm'),
+    (4, 'bow',       'Bow'),
+    (5, 'catalyst',  'Catalyst');
